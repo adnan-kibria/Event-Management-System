fb2b80eb7043d266587342df4c6ca3c6a3406765
adnan-kibria <adnan.official.rb000@gmail.com>
2025-08-31 11:03:53 +0600

    side nav-bar and top-bar added and styled

---
+++ view/event-report.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Event Report</title>
+    <link rel="stylesheet" href="../css/across-style.css">
+</head>
+<body>
+    <div class="side-nav">
+        <h2>Event Report</h2>
+        <hr>
+        <ul>
+            <li><a href="./admin-dashboard.php">Home</a></li>
+            <li><a href="./user-management.php">User Management</a></li>
+            <li><a href="./event-management.php">Event Management</a></li>
+            <li><a href="./reg-management.php">Ticket Management</a></li>
+            <li><a class="active" href="./event-report.php">Event Report</a></li>
+            <li><a href="./announcement.php">Announcement</a></li>
+        </ul>
+    </div>
+    <div class="main">
+        <div class="top-bar">
+            <div class="top">
+                <div class="search">
+                    <input type="text" placeholder="Search...">
+                    <button>Search</button>
+                </div> 
+            </div>
+            <div class="bottom">
+                <div class="user-intro">
+                    <img src="../images/admin-dp.jpg" alt="User Image">
+                    <div>
+                        <p>Hi there,</p>
+                        <h3>Admin (@admin)</h3>
+                    </div>
+                </div>
+                <div class="user-info">
+                    <button>Inbox</button>
+                    <div class="wrap">
+                        <div class="profile">
+                            <img src="../images/admin-dp.jpg" alt="admin-dp">
+                            <span>Admin</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
