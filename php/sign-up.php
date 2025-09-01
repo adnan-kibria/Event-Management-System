ec8dace6764ade9792afae8bb9aa403047d1e425
adnan-kibria <adnan.official.rb000@gmail.com>
2025-09-01 08:29:18 +0600

    Automatic role assign variable is placed

--- php/sign-up.php
+++ php/sign-up.php
@@ -9,13 +9,15 @@
         $confirm_password = trim($_POST['confirm-password'] ?? '');
         $terms = isset($_POST['terms']);
 
+        $role = "participant";
+
         if(empty($name) || !preg_match("/^[A-Za-z\s]+$/", $name) || empty($username) || !preg_match("/^[A-Za-z0-9_]+$/", $username) || empty($email) || filter_var($email, FILTER_VALIDATE_EMAIL) === false || empty($password) || strlen($password) < 8 || empty($confirm_password) || $confirm_password !== $password || !$terms){
             echo "Fill up the form correctly!";
         }
         else{
             $hash_pass = password_hash($password, PASSWORD_DEFAULT);
 
-            $sql = "INSERT INTO participants (name, username, email, password) VALUES ('$name', '$username', '$email', '$hash_pass')";
+            $sql = "INSERT INTO participants (name, username, email, password, role) VALUES ('$name', '$username', '$email', '$hash_pass', '$role')";
 
             if($conn->query($sql) === TRUE){
                 echo "New record created successfully";
