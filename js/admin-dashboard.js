8df3e1c89117cd32de35c03aac6cdbc7ee698aba
adnan-kibria <adnan.official.rb000@gmail.com>
2025-09-07 12:28:51 +0600

    Conver from fetch API to traditional AJAX

--- js/admin-dashboard.js
+++ js/admin-dashboard.js
@@ -1,8 +1,21 @@
-fetch("../php/adminDashboard.php")
-    .then(response => response.json())
-    .then(data => {
-        document.getElementById("total-users").innerText = data.total_users;
-        document.getElementById("total-events").innerText = data.total_events;
-        document.getElementById("total-registrations").innerText = data.total_registrations;
-    })
-    .catch(error => console.error("Error fetching dashboard data:", error));
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "../php/adminDashboard.php", true);
+
+xhr.onreadystatechange = function(){
+    if(xhr.readyState === 4 && xhr.status === 200){
+        try{
+            var data = JSON.parse(xhr.responseText);
+            document.getElementById("total-users").innerHTML = data.total_users;
+            document.getElementById("total-events").innerHTML = data.total_events;
+            document.getElementById("total-registrations").innerHTML = data.total_registrations;
+        }
+        catch(e){
+            console.error("Error parsing JSON: ", e);
+        }
+    }
+}
+
+xhr.onerror = function(){
+    console.error("Error fetching dashboard data");
+}
+xhr.send();
