c52a8e6da99b1461b75f2110d0a126d07cb4f620
adnan-kibria <adnan.official.rb000@gmail.com>
2025-09-08 20:02:46 +0600

    !Important elements added Defined null for both remember_token and token_expiry in sign-up.php Added uniqueness check for email and username in sign-up.php Prevent sql injection by using prepared statements in sign-up.php

--- php/sign-up.php
+++ php/sign-up.php
@@ -13,17 +13,32 @@
 
         if(empty($name) || !preg_match("/^[A-Za-z\s]+$/", $name) || empty($username) || !preg_match("/^[A-Za-z0-9_]+$/", $username) || empty($email) || filter_var($email, FILTER_VALIDATE_EMAIL) === false || empty($password) || strlen($password) < 8 || empty($confirm_password) || $confirm_password !== $password || !$terms){
             echo "Fill up the form correctly!";
+            exit();
         }
         else{
+            $stmt_check = $conn->prepare("SELECT * FROM participants WHERE email = ? OR username = ?");
+            $stmt_check->bind_param("ss", $email, $username);
+            $stmt_check->execute();
+            $result_check = $stmt_check->get_result();
+            if($result_check->num_rows > 0){
+                echo "Email or Username already exists!";
+                exit();
+            }
+
             $hash_pass = password_hash($password, PASSWORD_DEFAULT);
 
-            $sql = "INSERT INTO participants (name, username, email, password, role) VALUES ('$name', '$username', '$email', '$hash_pass', '$role')";
+            $remember_token = null;
+            $token_expiry = null;
 
-            if($conn->query($sql) === TRUE){
+            $sql = "INSERT INTO participants (name, username, email, password, role, remember_token, token_expiry) VALUES (?, ?, ?, ?, ?, ?, ?)";
+            $stmt = $conn->prepare($sql);
+            $stmt->bind_param("sssssss", $name, $username, $email, $hash_pass, $role, $remember_token, $token_expiry);
+
+            if($stmt->execute() === TRUE){
                 echo "New record created successfully";
             }
             else{
-                echo "Error: " . $sql . "<br>" . $conn->error;
+                echo "Error: " .$stmt->error;
             }
         } 
     }
