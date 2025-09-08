ff43c32fc06de974638506677b31c0928159a4c8
adnan-kibria <adnan.official.rb000@gmail.com>
2025-09-08 20:05:21 +0600

    Rename remember-me to remember_me

--- php/sign-out.php
+++ php/sign-out.php
@@ -2,7 +2,7 @@
     session_start();
     session_unset();
     session_destroy();
-    setcookie("remember-token", "", time() - 3600, "/");
+    setcookie("remember_token", "", time() - 3600, "/");
     header("Location: ../view/sign-up-sign-in.php");
     exit();
 ?>
