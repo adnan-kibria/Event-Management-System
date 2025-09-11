4c6ef3bacab3fbff7f454702a0c94848407ac076
adnan-kibria <adnan.official.rb000@gmail.com>
2025-09-11 10:54:57 +0600

    Fetched user-data from JSON and show data as table format

---
+++ js/user-manage.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function(){
+    const user_data = document.getElementById("user-data");
+
+    function fetchUserData(){
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", "../php/user-manage.php", true);
+        xhr.onreadystatechange = function(){
+            if(xhr.readyState === 4 && xhr.status === 200){
+                try{
+                    var data = JSON.parse(xhr.responseText);
+                    user_data.innerHTML = "";
+                    data.forEach(function(user){
+                        var row = document.createElement("tr");
+                        row.innerHTML = `<td>${user.id}</td>
+                                        <td>${user.name}</td>
+                                        <td>${user.username}</td>
+                                        <td>${user.email}</td>
+                                        <td>${user.phone}</td>
+                                        <td>
+                                            <button>View</button>
+                                        </td>`;
+                        user_data.appendChild(row);
+                    });
+                } catch(e){
+                    console.error("Error parsing JSON:", e);
+                }
+            }
+        };
+        xhr.send();
+    }
+
+    fetchUserData();
+});
