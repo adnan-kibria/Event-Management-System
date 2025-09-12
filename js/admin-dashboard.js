08e6075208512defaf75a917b915f73e31832ed6
adnan-kibria <adnan.official.rb000@gmail.com>
2025-09-13 00:32:32 +0600

    Retrived and show data of announcement

--- js/admin-dashboard.js
+++ js/admin-dashboard.js
@@ -19,3 +19,28 @@ xhr.onerror = function(){
     console.error("Error fetching dashboard data");
 }
 xhr.send();
+
+const announcement = document.querySelector(".announce-list");
+const xhr1 = new XMLHttpRequest();
+xhr1.open("GET", "../php/announce.php");
+xhr1.onreadystatechange = function(){
+    if(xhr1.readyState === 4 && xhr1.status === 200){
+        try{
+            let data = JSON.parse(xhr1.responseText);
+            announcement.innerHTML = "";
+            data.forEach(function(announce){
+                let announce_item = document.createElement("div");
+                announce_item.classList.add("announce-item");
+                announce_item.innerHTML = `
+                            <h3>${announce.announcement_title}</h3>
+                            <p>${announce.body}</p>`;
+                announcement.appendChild(announce_item);
+            });
+        }
+        catch(e){
+            console.error("Error parsing JSON: ", e);
+        }
+    }
+}
+xhr1.send();
+
